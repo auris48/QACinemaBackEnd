57b61b41a2c9e2148e0dd54cfc16ce61a0325e92
rossmorr1 <rossmorr97@gmail.com>
2022-09-09 11:51:29 +0100

    added user entity

--- model/User.js
+++ model/User.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
+// import { isEmail } from 'validator';
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String
-});
+    username: {
+        type: String,
+        minlength: [8, "username must be 8 characters or more"],
+        maxlength: [20, "username must be 20 characters or less"]
+    },
+    password: {
+        type: String,
+        // required: true,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    },
+    email: {
+        type:String,
+        validate:{
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email',
+            isAsync: false
+    }
+}});
 
 // Adds a hash and salt field, and uses these to encrypt the password
 // - also adds some helper methods for validating users
